ba3f890a46957e3cd1831f4abbc7302670f0388e
Mikołaj Kondela <mkondela.formal@gmail.com>
2024-06-16 18:31:13 +0200

    Add wip tag to test cards

--- src/components/Tests.jsx
+++ src/components/Tests.jsx
@@ -9,6 +9,7 @@ export default function Tests() {
   return (
     <div className="tests-card">
       <div className="tests-container">
+        {/* br is for wip to be positioned correctly */}
         <a href="/reactiontime">
           <div id="reaction-time" className="tests-item">
             <img src={ReactionTimeTest} alt="test-image" />
@@ -17,7 +18,7 @@ export default function Tests() {
           </div>
         </a>
         <a href="/sequencememory">
-          <div id="sequence-memory" className="tests-item">
+          <div id="sequence-memory" className="tests-item wip">
             <img src={SequenceMemoryTest} alt="test-image" />
             <h3 className="test-title">Sequence Memory</h3>
             <p className="test-description">
@@ -26,25 +27,25 @@ export default function Tests() {
           </div>
         </a>
         <a href="/aimtrainer">
-          <div id="aim-trainer" className="tests-item">
+          <div id="aim-trainer" className="tests-item wip">
             <img src={AimTrainerTest} alt="test-image" />
             <h3 className="test-title">Aim Trainer</h3>
             <p className="test-description">
-              How quickly can you hit all the targets?
+              How quickly can you hit all the targets? <br /> <br />
             </p>
           </div>
         </a>
         <a href="/numbermemory">
-          <div id="number-memory" className="tests-item">
+          <div id="number-memory" className="tests-item wip">
             <img src={NumberMemoryTest} alt="test-image" />
             <h3 className="test-title">Number Memory</h3>
             <p className="test-description">
-              Remember the longest number you can.
+              Remember the longest number you can. <br /> <br />
             </p>
           </div>
         </a>
         <a href="/verbalmemory">
-          <div id="verbal-memory" className="tests-item">
+          <div id="verbal-memory" className="tests-item wip">
             <img src={VerbalMemoryTest} alt="test-image" />
             <h3 className="test-title">Verbal Memory</h3>
             <p className="test-description">
@@ -53,11 +54,11 @@ export default function Tests() {
           </div>
         </a>
         <a href="/chimptest">
-          <div id="chimp-test" className="tests-item">
+          <div id="chimp-test" className="tests-item wip">
             <img src={SequenceMemoryTest} alt="test-image" />
             <h3 className="test-title">Chimp Test</h3>
             <p className="test-description">
-              Are you smarter than a chimpanzee?
+              Are you smarter than a chimpanzee? <br /> <br />
             </p>
           </div>
         </a>
